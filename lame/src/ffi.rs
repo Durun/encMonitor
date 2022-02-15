0ad8b386d77ac7090bc21a03549beb6f9820db53
Durun <negiigainuki@gmail.com>
2022-02-15 15:48:40 +0900

    add ffi functions

--- lame/src/ffi.rs
+++ lame/src/ffi.rs
@@ -1,6 +1,24 @@
-use std::os::raw::{c_int, c_void};
+use std::os::raw::{c_int, c_void, c_ulong, c_short};
 
 pub type LamePtr = *mut c_void;
+pub type HipPtr = *mut c_void;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+#[derive(Debug)]
+#[derive(Default)]
+pub struct mp3data_struct {
+    pub header_parsed: c_int,
+    pub stereo: c_int,
+    pub samplerate: c_int,
+    pub bitrate: c_int,
+    pub mode: c_int,
+    pub mode_ext: c_int,
+    pub framesize: c_int,
+    pub nsamp: c_ulong,
+    pub totalframes: c_int,
+    pub framenum: c_int,
+}
 
 #[link(name="mp3lame")]
 extern "C" {
@@ -27,4 +45,25 @@ extern "C" {
                                    mp3buf: *mut u8, mp3buf_size: c_int) -> c_int;
     pub fn lame_get_encoder_delay(ptr: LamePtr) -> c_int;
     pub fn lame_get_encoder_padding(ptr: LamePtr) -> c_int;
+
+    pub fn hip_decode_init() -> HipPtr;
+    pub fn hip_decode_exit(ptr: HipPtr) -> c_int;
+    pub fn hip_decode1_headersB(ptr: HipPtr,
+                                mp3buf: *const u8,
+                                mp3buf_size: usize,
+                                pcm_l: *mut c_short,
+                                pcm_r: *mut c_short,
+                                mp3data: *mut mp3data_struct,
+                                enc_delay: *mut c_int,
+                                enc_padding: *mut c_int) -> c_int;
+    pub fn hip_decode1(ptr: HipPtr,
+                       mp3buf: *const u8,
+                       mp3buf_size: usize,
+                       pcm_l: *mut c_short,
+                       pcm_r: *mut c_short) -> c_int;
+    pub fn hip_decode(ptr: HipPtr,
+                      mp3buf: *const u8,
+                      mp3buf_size: usize,
+                      pcm_l: *mut c_short,
+                      pcm_r: *mut c_short) -> c_int;
 }
